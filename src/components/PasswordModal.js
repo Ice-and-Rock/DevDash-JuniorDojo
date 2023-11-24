b28a1913c85ff511eebb3c4506f59eaba207469a
nick v <nvalente.electrics@gmail.com>
2023-11-24 22:12:25 +0100

    added modal and props to modal passwordAccess component

---
+++ src/components/PasswordModal.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+const PasswordModal = ({ showModal, handleClose }) => {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+      
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    
+  );
+}
+
+export default PasswordModal;
--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Test from "./Test";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
+import Button from "react-bootstrap/Button";
+import PasswordModal from './PasswordModal';
+
+
 
 const TestPage = () => {
   const { subject } = useParams();
+  const [passwordAccess, setPasswordAccess] = useState(false)
+  
+  //MODAL COMPONENT
+  const [showModal, setShowModal] = useState(false);
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
 
   return (
-    <div>
-      <h1>{subject.toUpperCase()} Quiz</h1>
-      <Test subject={subject} />
-    </div>
+    <Container>
+
+{!passwordAccess && (
+  <Card style={{ width: '18rem' }}>
+      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      <Card.Body>
+        <Card.Title>{subject.toUpperCase()} QUIZ</Card.Title>
+        <Card.Text>Please click on the button below to start</Card.Text>
+        <>
+      <Button variant="primary" onClick={handleShow}>
+        Enter Password
+      </Button>
+      <PasswordModal showModal={showModal} handleClose={handleClose} />
+    </>
+      </Card.Body>
+    </Card>
+    )}
+    {passwordAccess && (
+      <>
+        <h1>{subject.toUpperCase()} QUIZ...</h1>
+        <Test subject={subject} />
+      </>
+    )}
+  </Container>
   );
 };
 
