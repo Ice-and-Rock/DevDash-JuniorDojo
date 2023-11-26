2fb50d6f5fa9fd9ed73b5da031910e55c034cb9f
nick v <nvalente.electrics@gmail.com>
2023-11-26 21:21:33 +0100

    finished initial stages of modal. Set password and it works, showing test data. next, create a questionaire component for UI

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,30 +1,45 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
-
-import { Example } from "./Example";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import {Example} from "./Example"; // Assuming Example is a React Bootstrap component
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Your Quiz App</h1>
-      <p>Choose your topic:</p>
-      <ul>
-        <li>
-          <Link to="/test/react">React</Link>
-        </li>
-        <li>
-          <Link to="/test/javascript">Javascript</Link>
-        </li>
-        <li>
-          <Link to="/test/css">CSS</Link>
-        </li>
-        <li>
-          <Link to="/test/sql">SQL</Link>
-        </li>
-      </ul>
-
-    </div>
+    <Container className="text-center mt-5">
+      <h1>Welcome to Your Interview Practice App!</h1>
+      <p className="lead">Choose your topic:</p>
+      <Row className="justify-content-center">
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <Link to="/test/react" className="text-decoration-none">
+            <Button variant="primary" >
+              React
+            </Button>
+          </Link>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <Link to="/test/javascript" className="text-decoration-none">
+            <Button variant="success" >
+              Javascript
+            </Button>
+          </Link>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <Link to="/test/css" className="text-decoration-none">
+            <Button variant="info" >
+              CSS
+            </Button>
+          </Link>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <Link to="/test/sql" className="text-decoration-none">
+            <Button variant="warning" >
+              SQL
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+      {/* <Example /> */}
+    </Container>
   );
 };
 
--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -4,44 +4,46 @@ import Test from "./Test";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/esm/Container";
 import Button from "react-bootstrap/Button";
-import PasswordModal from './PasswordModal';
-
-
+import PasswordModal from "./PasswordModal";
 
 const TestPage = () => {
   const { subject } = useParams();
-  const [passwordAccess, setPasswordAccess] = useState(false)
-  
+  const [passwordAccess, setPasswordAccess] = useState(false);
+
   //MODAL COMPONENT
   const [showModal, setShowModal] = useState(false);
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
 
+  const grantAccess = () => {
+    setPasswordAccess(true);
+    handleClose();
+  };
+
   return (
     <Container>
-
-{!passwordAccess && (
-  <Card style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-      <Card.Body>
-        <Card.Title>{subject.toUpperCase()} QUIZ</Card.Title>
-        <Card.Text>Please click on the button below to start</Card.Text>
+      {!passwordAccess && (
+        <Card style={{ width: "18rem" }}>
+          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+          <Card.Body>
+            <Card.Title>{subject.toUpperCase()} QUIZ</Card.Title>
+            <Card.Text>Please click on the button below to start</Card.Text>
+            <>
+              <Button variant="primary" onClick={handleShow}>
+                Enter Password
+              </Button>
+              <PasswordModal showModal={showModal} handleClose={handleClose} grantAccess={grantAccess} />
+            </>
+          </Card.Body>
+        </Card>
+      )}
+      {passwordAccess && (
         <>
-      <Button variant="primary" onClick={handleShow}>
-        Enter Password
-      </Button>
-      <PasswordModal showModal={showModal} handleClose={handleClose} />
-    </>
-      </Card.Body>
-    </Card>
-    )}
-    {passwordAccess && (
-      <>
-        <h1>{subject.toUpperCase()} QUIZ...</h1>
-        <Test subject={subject} />
-      </>
-    )}
-  </Container>
+          <h1>{subject.toUpperCase()} QUIZ...</h1>
+          <Test subject={subject} />
+        </>
+      )}
+    </Container>
   );
 };
 
