5d772e881652e132f612f13a3f68ddb97ef99057
nick v <nvalente.electrics@gmail.com>
2023-12-29 09:03:39 +0100

    finished the render of user Results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,8 +6,8 @@ const Results = ({ userAnswers, responseData }) => {
 
   // NOTES
   // userAnswers is an Object, needs Object.keys for .MAP()
-  // combine the index of both .map() methods 
-    // colour the questions depending on this combination
+  // combine the index of both .map() methods
+  // colour the questions depending on this combination
 
   return (
     // <div>
@@ -33,34 +33,35 @@ const Results = ({ userAnswers, responseData }) => {
     // </div>
 
     <div>
-  <Card.Body>
-  <Card.Text>These were your answers</Card.Text>
-    {Object.keys(userAnswers).map((answerKey, index) => (
-      <div
-        key={index}
-        style={{ color: userAnswers[answerKey] ? 'green' : 'red' }}
-      >
-        {index}: {userAnswers[answerKey].toString()}
-      </div>
-    ))}
-  </Card.Body>
+      <Card.Body>
+        <Card.Text>These were your answers:</Card.Text>
+        {Object.keys(userAnswers).map((answerBoolean, index) => (
+          <div key={index}>
+            {/* Render emojis if answerKey is "true/false" */}
+            {index + 1}: {userAnswers[answerBoolean] ? "✅" : "❌"}
+          </div>
+        ))}
+      </Card.Body>
 
-  <Card.Body>
-  <Card.Text>The correct answers, with your incorrect answers in <span style={{ color: 'red', fontWeight: 'bold' }}>RED</span></Card.Text>
-    {responseData.questions.map((questionData, index) => (
-      <div key={index}>
-        <Card.Title>Question {index + 1}:</Card.Title>
-        <Card style={{ background: userAnswers[index] ? 'lightgreen' : 'pink' }} className="m-2 p-2">
-          <Card.Title>
-            {questionData.question}
-          </Card.Title>
-          <Card.Text>{questionData.answers.correct}</Card.Text>
-        </Card>
-      </div>
-    ))}
-  </Card.Body>
-</div>
-
+      <Card.Body>
+        <Card.Text>
+          The correct answers, with your incorrect answers in{" "}
+          <span style={{ color: "red", fontWeight: "bold" }}>RED</span>
+        </Card.Text>
+        {responseData.questions.map((questionData, index) => (
+          <div key={index}>
+            <Card.Title>Question {index + 1}:</Card.Title>
+            <Card
+              style={{ background: userAnswers[index] ? "lightgreen" : "pink" }}
+              className="m-2 p-2"
+            >
+              <Card.Title>{questionData.question}</Card.Title>
+              <Card.Text>{questionData.answers.correct}</Card.Text>
+            </Card>
+          </div>
+        ))}
+      </Card.Body>
+    </div>
   );
 };
 
