5f617f3eb5781eb69764d05ce39b62a2eb237476
nick v <nvalente.electrics@gmail.com>
2024-01-02 20:36:22 +0100

    confetti done, but cant control the zIndex. c'est la vie

--- src/components/ConfettiPointer.js
+++ src/components/ConfettiPointer.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 
+// Notes:
+// zIndex doesn't work!
+// - use this site to see Docs: https://www.npmjs.com/package/react-dom-confetti
 
-const ConfettiPointer = ({ active, numberOfPieces, gravity, wind }) => {
+const ConfettiPointer = ({ active, numberOfPieces, gravity, wind, zIndex }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
@@ -29,6 +32,7 @@ const ConfettiPointer = ({ active, numberOfPieces, gravity, wind }) => {
       numberOfPieces={numberOfPieces || 100}
       gravity={gravity || 0.1}
       wind={wind || 0.05}
+      zIndex={zIndex || 1}
     />
   );
 };
